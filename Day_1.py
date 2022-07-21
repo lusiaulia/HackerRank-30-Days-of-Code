e2939540465cf8588de2a4c6bdab07f43f547021
Lusi Aulia <99398744+lusiaulia@users.noreply.github.com>
2022-07-21 08:52:45 +0700

    Day_1
    About Data Types

---
+++ Day_1.py
@@ -0,0 +1,14 @@
+#Data Type
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+k = int(input())
+l = float(input())
+m = str(input())
+
+# Print the sum of both integer variables on a new line.
+print(i + k)
+print(d + l)
+print(s + m)
